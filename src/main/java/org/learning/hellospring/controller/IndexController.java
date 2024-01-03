5031c36c29e36e813ab973324e1c34d0ddba7d78
Peppe2496 <giuseppemusellajob@gmail.com>
2024-01-03 14:56:14 +0100

    finished

--- src/main/java/org/learning/hellospring/controller/IndexController.java
+++ src/main/java/org/learning/hellospring/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Random;
 
@@ -12,9 +13,9 @@ import java.util.Random;
 public class IndexController {
 
     @GetMapping
-    public String index(Model model){
+    public String index(@RequestParam int maxNumber, Model model){
         Random random = new Random();
-        int randomNumber = random.nextInt(1,6);
+        int randomNumber = random.nextInt(1,maxNumber);
         model.addAttribute("randomNumber", randomNumber);
         return "index";
 
